4c68cf4954a4cdfb387fc52f446f0434aed2d7d3
Sakib Imam <sakibdrmc2015@gmail.com>
2026-09-08 04:14:49 +0600

    Remove comments and retain user creation SQL

--- UserCreation.sql
+++ UserCreation.sql
@@ -1,5 +1,3 @@
-1. /*This is the logical SQL equivalent for creating and granting privileges to the read-only user 'hustleup_reader' for local host. 
-The user was created via the Always Data admin panel due to hosting restrictions.*/
 
 CREATE USER 'hustleup_reader'@'localhost' IDENTIFIED BY 'passsword_read';
 GRANT SELECT ON hustleup_db.* TO 'hustleup_reader'@'localhost';
